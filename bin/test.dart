5752cd90f4d5507362b020be6a54cc9620d0aefb
Sigurd Meldgaard <sigurdm@google.com>
2019-12-15 11:05:49 +0100

    more work

--- bin/test.dart
+++ bin/test.dart
@@ -1,36 +1,32 @@
 import 'package:retriever/retriever.dart';
 import 'package:http/http.dart';
+import 'package:async/async.dart';
 
-  final retriever = new PooledRetriever<Uri, String>();
-
-Future<String> getUrl(Uri url) async {
-  String body;
-  try {
-  body = (await get(url)).body;
-  } catch (e) {
-    print("error $e");
-  } finally {}
-  RegExp(r'<a href="(.[^"]*)"').allMatches(body ?? '').forEach((l) {
-  //  print("$url ${l[1]}");
-    try {
-    enqueue(Uri.parse(l[1]));
-    } finally {}
-  });
-  return body;
-}
-
-enqueue(Uri uri) {
-    final task = Task<Uri, String>(
-     uri,
-      getUrl,
-      (url) {},
-      1);
-      retriever.enqueue(task);
-}
-
-main() {
+main() async {
   print("hej");
+  var i = 0;
+  final retriever = new Retriever<Uri, String>((url, retriever) {
+    fetch() async {
+      String body;
+      try {
+        print('${i++} fetching $url');
+        body = await Future.any(
+            [Future.delayed(Duration(seconds: 2), () async => ""), read(url)]);
+      } catch (e) {
+        print("error $e");
+      } finally {}
+      RegExp(r'<a href="(http[^"]*)"').allMatches(body ?? '').forEach((l) {
+        print("$url ${Uri.parse(l[1])}");
+        retriever.enqueue(Uri.parse(l[1]));
+      });
+      return body;
+    }
 
-  enqueue(Uri.parse('https://github.com/gskinnerTeam/flutter_vignettes'));
+    return CancelableOperation.fromFuture(fetch());
+  }, maxConcurrentOperations: 30);
+
+  retriever
+      .enqueue(Uri.parse('https://github.com/gskinnerTeam/flutter_vignettes'));
   print("farvel");
+  await Future.delayed(Duration(days: 1));
 }
--- lib/retriever.dart
+++ lib/retriever.dart
@@ -1,65 +1,99 @@
 import 'dart:async';
 
 import "package:pool/pool.dart";
-import "package:collection/collection.dart";
+import "package:async/async.dart";
+import "dart:collection";
 
-class Task<K, V> extends Comparable {
-  final K key;
-  final Future<V> Function(K) get;
-  final void Function(K) cancel;
-  final int priority;
-  Task(this.key, this.get, this.cancel, this.priority);
-
-  int compareTo(other) => priority.compareTo(other.priority);
-}
-
-class PooledRetriever<K, V> {
+/// Handles rate limited scheduling of tasks.
+///
+/// Designed to allow prefetching tasks tht will likely be needed
+/// later with [enqueue].
+class Retriever<K, V> {
+  final CancelableOperation<V> Function(K, Retriever) _get;
   final Map<K, Completer<V>> _cache = Map<K, Completer<V>>();
-  final PriorityQueue<Task<K, V>> queue = PriorityQueue<Task<K, V>>();
-  final Pool pool = Pool(5);
-  final Map<K, Task> active = <K, Task>{};
+
+  /// Operations that are waiting to run.
+  final Queue<K> _queue = Queue<K>();
+
+  final Pool _pool;
+
+  /// The active operations
+  final Map<K, CancelableOperation<V>> _active = <K, CancelableOperation<V>>{};
   bool started = false;
-  void process() async {
+
+  Retriever(this._get, {maxConcurrentOperations = 10})
+      : _pool = Pool(maxConcurrentOperations);
+
+  /// Starts operations from the beginning of the queue.
+  void _process() async {
+    assert(!started);
     started = true;
-    while (started && queue.isNotEmpty) {
-      print("**** ${active.length}");
-      final resource = await pool.request();
-      if (!started) break;
-      final task = queue.removeFirst();
-      final completer = _cache.putIfAbsent(task.key, () => Completer());
-      // Already done.
-      if (completer.isCompleted) continue;
-      // Already actively retrieving.
-      if (active.containsKey(task.key)) continue;
-      active[task.key] = task;
-      task.get(task.key).then((result) {
-        completer.complete(result);
-      }, onError: (error, stackTrace) {
-        completer.completeError(error, stackTrace);
-      }).whenComplete(() {
+    while (_queue.isNotEmpty) {
+      final resource = await _pool.request();
+      // This checks if [stop] has been called while waiting for a resource.
+      if (!started) {
         resource.release();
-        active.remove(task.key);
-      });
+        break;
+      }
+      // Take the highest priority task from the queue.
+      final task = _queue.removeFirst();
+      // Create or get the completer to deliver the result to.
+      final completer = _cache.putIfAbsent(task, () => Completer());
+      // Already done or already scheduled => do nothing.
+      if (completer.isCompleted || _active.containsKey(task)) {
+        resource.release();
+        continue;
+      }
+
+      // Schedule task.
+      final operation = _get(task, this);
+      _active[task] = operation;
+      operation
+          .then((result) {
+            completer.complete(result);
+          }, onError: (error, stackTrace) {
+            completer.completeError(error, stackTrace);
+          })
+          .value
+          .whenComplete(() {
+            resource.release();
+            _active.remove(task);
+          });
     }
     started = false;
   }
 
+  /// Cancels all active computations, and clears the queue.
   void stop() {
+    // Stop the processing loop
     started = false;
-    for (final task in active.values) {
-      task.cancel(task.key);
+    // Cancel all active operatios
+    for (final operation in _active.values) {
+      operation.cancel();
     }
+    // Do not process anymore.
+    _queue.clear();
   }
 
-  void enqueue(Task task) {
-    queue.add(task);
-    if (!started) {
-      process();
-    }
+  // Puts [task] in the back of the work queue.
+  void enqueue(K task) {
+    _queue.addLast(task);
+    if (!started) _process();
   }
 
-  Future<V> fetch(Task task) {
-    enqueue(task);
-    return _cache.putIfAbsent(task.key, () => Completer()).future;
+  /// Returns the result of [_get]ting [task].
+  ///
+  /// If [task] is already done, the cached result will be returned.
+  /// If [task] is not yet active, it will go to the front of the work queue
+  /// to be scheduled when there are free resources.
+  Future<V> get(K task) {
+    final completer = _cache.putIfAbsent(task, () => Completer());
+    if (!completer.isCompleted) {
+      // We don't worry about adding the same task twice.
+      // It will get dedupped by the [_process] loop.
+      _queue.addFirst(task);
+      if (!started) _process();
+    }
+    return completer.future;
   }
 }
